6c6f91313e6d4dbc66ee9b3e605f5e16cc83af1d
raflie <30603031+ye-yu@users.noreply.github.com>
2026-05-12 09:40:23 +0000

    Add test for inner object

--- tests/hot.test.ts
+++ tests/hot.test.ts
@@ -2,23 +2,32 @@ import { beforeEach, describe, it } from 'node:test';
 import assert from 'node:assert/strict';
 import { clearHotModules, hotModule, hotReplace } from '../src/hot.ts';
 
+// fixtures
+class BaseClassA {
+    method() {
+        return "BaseClassA"
+    }
+}
+
+class BaseClassB {
+    method() {
+        return "BaseClassB"
+    }
+}
+
+function methodA() {
+    return "methodA"
+}
+
+function methodB() {
+    return "methodB"
+}
+
 describe('hotModule', () => {
     beforeEach(clearHotModules)
 
     describe('class', () => {
         it('should be able to replace base class', () => {
-            class BaseClassA {
-                method() {
-                    return "BaseClassA"
-                }
-            }
-
-            class BaseClassB {
-                method() {
-                    return "BaseClassB"
-                }
-            }
-
             const HotBaseClassA = hotModule(BaseClassA)
             const instance = new HotBaseClassA()
             assert.equal(instance.method(), "BaseClassA")
@@ -31,18 +40,6 @@ describe('hotModule', () => {
         })
 
         it('should be able to inherit hot replaced instances', () => {
-            class BaseClassA {
-                method() {
-                    return "BaseClassA"
-                }
-            }
-
-            class BaseClassB {
-                method() {
-                    return "BaseClassB"
-                }
-            }
-
             const HotBaseClassA = hotModule(BaseClassA)
 
             class CompositeClass {
@@ -70,13 +67,6 @@ describe('hotModule', () => {
 
     describe('function', () => {
         it('should be able to replace function', () => {
-            function methodA() {
-                return "methodA"
-            }
-            function methodB() {
-                return "methodB"
-            }
-
             const hotMethodA = hotModule(methodA)
             assert.equal(hotMethodA(), "methodA")
 
@@ -87,26 +77,6 @@ describe('hotModule', () => {
 
     describe('object', () => {
         it('should be able to replace object properties', () => {
-            class BaseClassA {
-                method() {
-                    return "BaseClassA"
-                }
-            }
-
-            class BaseClassB {
-                method() {
-                    return "BaseClassB"
-                }
-            }
-
-            function methodA() {
-                return "methodA"
-            }
-
-            function methodB() {
-                return "methodB"
-            }
-
             const obj = {
                 BaseClassA,
                 methodA,
@@ -122,6 +92,32 @@ describe('hotModule', () => {
 
             assert.equal(new hotObj.BaseClassA().method(), "BaseClassB")
             assert.equal(hotObj.methodA(), "methodB")
+        })
+
+        it('shoudl be able to replace nested object', () => {
+            const innerObj = {
+                instance: new BaseClassA(),
+                methodA,
+            }
+
+            const hotInnerObj = hotModule(innerObj)
+
+            const obj = {
+                BaseClassA,
+                methodA,
+                innerObj: hotInnerObj,
+            }
+
+            const hotObj = hotModule(obj)
+
+            assert.equal(hotObj.innerObj.instance.method(), "BaseClassA")
+            assert.equal(hotObj.innerObj.methodA(), "methodA")
+
+            hotReplace(BaseClassA, BaseClassB)
+            hotReplace(methodA, methodB)
+
+            assert.equal(hotObj.innerObj.instance.method(), "BaseClassB")
+            assert.equal(hotObj.innerObj.methodA(), "methodB")
         })
     })
 })
